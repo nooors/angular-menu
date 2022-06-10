95ef831e9f23d25bd428eaef69f48d238feb84aa
Jose Nortes <nooors@gmail.com>
2022-06-10 12:00:55 +0200

    initial commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [{ path: 'layout', loadChildren: () => import('./layout/layout.module').then(m => m.LayoutModule) }];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],

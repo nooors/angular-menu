64eba6a092a241d86d78daf03cada9ba26d2996f
Jose Nortes <nooors@gmail.com>
2022-06-11 11:22:18 +0200

    component navigator started

--- src/app/layout/navigator/navigator.component.ts
+++ src/app/layout/navigator/navigator.component.ts
@@ -1,15 +1,19 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 
 @Component({
-  selector: 'app-navigator',
-  templateUrl: './navigator.component.html',
-  styleUrls: ['./navigator.component.scss']
+  selector: "app-navigator",
+  templateUrl: "./navigator.component.html",
+  styleUrls: ["./navigator.component.scss"],
 })
 export class NavigatorComponent implements OnInit {
+  links: string[] = [
+    "Departaments",
+    "Families",
+    "Languages",
+    "Products",
+    "Product Types",
+  ];
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
